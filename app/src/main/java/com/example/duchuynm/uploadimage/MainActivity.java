ee7437b1295d4a1c7018b4a23cab2ae6e9323376
duchuy0990 <duchuy0990@gmail.com>
2018-08-06 11:58:20 +0700

    fix bugs

--- app/src/main/java/com/example/duchuynm/uploadimage/MainActivity.java
+++ app/src/main/java/com/example/duchuynm/uploadimage/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.duchuynm.uploadimage;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -14,8 +17,12 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -24,14 +31,17 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.MediaController;
-import android.widget.ScrollView;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -40,26 +50,65 @@ public class MainActivity extends AppCompatActivity {
     Button chooseImage, uploadImageToServer;
     EditText imageName;
     ImageView imageSelected;
-    FrameLayout frameLayout;
     MediaController mediaController = null;
-    ScrollView scrollView;
+    FrameLayout frameLayout;
+    ProgressDialog progressDialog;
+    private VideoView videoSelected;
+    MenuItem optionMenu;
 
+    private static final String folder = Environment
+            .getExternalStorageDirectory()
+            .toString()
+            +"/UploadImage";
     private int REQUEST_CODE_GALLERY = 9999;
     private int REQUEST_CODE_CAMERA = 9998;
     private int REQUEST_CODE_VIDEO = 9997;
+    private int PIC_EDIT = 9995;
+    private int TYPE_IMAGE = 0;
+    private int TYPE_VIDEO = 1;
 
     Bitmap bitmap = null;
 
-    String getTextFromEditText = " ";
-
+    String getTextFromEditText = "";
     private String deviceName;
     private java.sql.Date currentTime;
+    private Uri uri = null;
+    private String path;
 
-    ProgressDialog progressDialog;
-    private VideoView videoSelected;
-    private Uri videoUri = null;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemEdit:
+                startActionEdit();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    private void startActionEdit() {
+        Intent editIntent = new Intent(
+                MainActivity.this,
+                DsPhotoEditorActivity.class);
+
+        editIntent.setData(uri);
+
+        editIntent.putExtra(
+                DsPhotoEditorConstants.DS_PHOTO_EDITOR_API_KEY,
+                "7fdf6963cfbf09a9a14e43c2fcd01af279ae7638");
+
+        editIntent.putExtra(
+                DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY,
+                "UploadImage");
+
+        startActivityForResult(editIntent,PIC_EDIT);
+    }
+
+    @SuppressLint("ResourceType")
     private void init() {
         chooseImage = findViewById(R.id.btnChooseImage);
         uploadImageToServer = findViewById(R.id.btnUploadtoServer);
@@ -67,6 +116,8 @@ public class MainActivity extends AppCompatActivity {
         imageSelected = findViewById(R.id.imageView);
         videoSelected = findViewById(R.id.videoView);
         frameLayout = findViewById(R.id.frame_layout_video);
+        frameLayout.setBackgroundColor(getResources().getColor(R.color.black));
+        optionMenu = findViewById(R.menu.my_menu);
     }
 
     @Override
@@ -74,11 +125,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-
         chooseImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showPictureDialog();
+                showActionDialog();
             }
         });
 
@@ -101,29 +151,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void uploadImageToServer() {
-        if(!checkInternet()) {
-            Toast.makeText(MainActivity.this,"Internet chua duoc bat",Toast.LENGTH_LONG).show();
-        }
-        else {
-            String filePath;
+        if (!checkInternet()) {
+            Toast.makeText(MainActivity.this, "Internet chua duoc bat", Toast.LENGTH_LONG).show();
+        } else {
             getDataToUpLoad();
-            if(bitmap != null) {
-                filePath = saveImage(getTextFromEditText, bitmap);
-            }
-            else {
-                filePath = saveVideo(getTextFromEditText, videoUri);
-            }
 
-            Log.e("name", getTextFromEditText);
-            Log.e("time", currentTime.toString());
-            Log.e("device", deviceName);
-            Log.e("filePath", filePath);
-
-            File file = new File(filePath);
-
-            final ftpConnection ftpconn = new ftpConnection(
-                    filePath) {
-
+            final ftpConnection ftpconn = new ftpConnection(path) {
                 @Override
                 protected void onPreExecute() {
                     super.onPreExecute();
@@ -134,17 +167,18 @@ public class MainActivity extends AppCompatActivity {
                 protected void onPostExecute(Object o) {
                     super.onPostExecute(o);
                     progressDialog.dismiss();
-                    if((boolean)o) {
-                        Toast.makeText(MainActivity.this,"successfully",Toast.LENGTH_LONG).show();
+                    if ((boolean) o) {
+                        Toast.makeText(MainActivity.this, "successfully", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "loi khong xac dinh", Toast.LENGTH_LONG).show();
                     }
-                    else {
-                        Toast.makeText(MainActivity.this,"loi khong xac dinh",Toast.LENGTH_LONG).show();
-                    }
-                    Intent i = new Intent(MainActivity.this,MainActivity.class);
+                    Intent i = new Intent(MainActivity.this, MainActivity.class);
                     startActivity(i);
                 }
             };
             ftpconn.execute();
+
+            File file = new File(path);
 
             JDBCconnection jdbcConnection = new JDBCconnection();
             jdbcConnection.execute(file.getName(), currentTime.toString(), deviceName);
@@ -157,18 +191,20 @@ public class MainActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(
                 MainActivity.this,
                 ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
         progressDialog.setTitle("Uploading");
         progressDialog.setMessage("please wait...");
         progressDialog.show();
     }
 
-    private void showPictureDialog() {
+    private void showActionDialog() {
         AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
         pictureDialog.setTitle("Select Action");
         String[] pictureDialogItems = {
                 "Photo Gallery",
                 "Camera",
-                "Video"};
+                "Video"
+        };
         pictureDialog.setItems(pictureDialogItems,
                 new DialogInterface.OnClickListener() {
                     @Override
@@ -189,68 +225,6 @@ public class MainActivity extends AppCompatActivity {
         pictureDialog.show();
     }
 
-    private void takeVideo() {
-        Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        startActivityForResult(videoIntent,REQUEST_CODE_VIDEO);
-    }
-
-    public String saveImage(String fileName, Bitmap b) {
-        File image = null;
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath().toString()+"/duchuy";
-        final File Dir = new File(path);
-        if(!Dir.exists()) {
-            Dir.mkdir();
-        }
-
-        fileName = ""+fileName+getTime();
-
-        try {
-                image = File.createTempFile(
-                        fileName,
-                        ".jpg",
-                        Dir);
-                FileOutputStream fos = new FileOutputStream(image);
-                b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                fos.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return image.getAbsolutePath();
-    }
-
-    public String saveVideo(String fileName, Uri uri) {
-        File video = null;
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath().toString()+"/duchuy";
-        final File Dir = new File(path);
-        if(!Dir.exists()) {
-            Dir.mkdir();
-        }
-
-        fileName = ""+fileName+getTime();
-
-        try {
-            video = File.createTempFile(
-                    fileName,
-                    ".mp4",
-                    Dir);
-
-            InputStream inputStream = getContentResolver().openInputStream(uri);
-            FileOutputStream fos = new FileOutputStream(video);
-
-            byte[] b = new byte[1024];
-            int len;
-
-            while ((len = inputStream.read(b)) >0 ) {
-                fos.write(b,0,len);
-            }
-            inputStream.close();
-            fos.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return video.getAbsolutePath();
-    }
-
     public String getTime() {
         String format = "yyyyMMddhhmmss";
         SimpleDateFormat df = new SimpleDateFormat(format);
@@ -259,9 +233,40 @@ public class MainActivity extends AppCompatActivity {
         return getTime;
     }
 
+    private File createFile(int type) {
+        String name = getTime();
+        File dir = new File(folder);
+        if(!dir.exists()) {
+            dir.mkdirs();
+        }
+        File des;
+        if(type == TYPE_IMAGE) {
+            des = new File(folder.toString()+"/"+name+".jpg");
+            return des;
+        }
+        else if(type == TYPE_VIDEO) {
+            des = new File(folder.toString()+"/"+name+".mp4");
+            return des;
+        }
+        return null;
+    }
+
     private void takePhotoFromCamera() {
         Intent accessCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File des = createFile(0);
+        Uri uri = Uri.fromFile(des);
+        accessCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        path = des.getAbsolutePath();
         startActivityForResult(accessCameraIntent,REQUEST_CODE_CAMERA);
+    }
+
+    private void takeVideo() {
+        Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        File des = createFile(1);
+        Uri uri = Uri.fromFile(des);
+        videoIntent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        path = des.getAbsolutePath();
+        startActivityForResult(videoIntent,REQUEST_CODE_VIDEO);
     }
 
     private void choosePhotoFromGallery() {
@@ -272,32 +277,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if((requestCode == REQUEST_CODE_CAMERA) && (resultCode == RESULT_OK)) {
-            invisibleVideoView();
-            visibleImageView();
-            visibleButton();
-            setDataImageView(data);
+        if(resultCode == RESULT_OK) {
+            if(requestCode == PIC_EDIT) {
+                Uri uri = data.getData();
+                imageSelected.setImageURI(uri);
+            }
+            if(requestCode == REQUEST_CODE_CAMERA) {
+                setDataImageView(path);
+                displayImage();
+                visibleButton();
+            }
+            else if(requestCode == REQUEST_CODE_GALLERY) {
+                uri = data.getData();
+                visibleButton();
+                displayImageOrVideoReturnedFromGallery(data);
+            }
+            else if(requestCode == REQUEST_CODE_VIDEO) {
+                uri = data.getData();
+                displayVideo();
+                visibleButton();
+                setDataVideoView();
+            }
         }
-        else if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK) {
-            visibleButton();
-            displayImageOrVideoReturnedFromGallery(data);
-        }
-        else if(requestCode == REQUEST_CODE_VIDEO && resultCode == RESULT_OK) {
-            bitmap = null;
-            displayVideo();
-            visibleButton();
-            displayVideoReturnedFromCamera(data);
-            videoUri = data.getData();
-        }
+
         imageName.setText("");
     }
 
-    private void displayVideoReturnedFromCamera(Intent data) {
-        Uri uri = Uri.parse(data.getData().toString());
-        videoSelected.setVideoURI(uri);
-        displayVideo();
+    private void setDataVideoView() {
+        videoSelected.setVideoPath(path);
     }
 
     private void visibleButton() {
@@ -305,43 +315,41 @@ public class MainActivity extends AppCompatActivity {
         uploadImageToServer.setVisibility(View.VISIBLE);
     }
 
-    private void setDataImageView(Intent data) {
-        bitmap = (Bitmap) data.getExtras().get("data");
+    private void setDataImageView(String data) {
+        bitmap = BitmapFactory.decodeFile(data);
         imageSelected.setImageBitmap(bitmap);
     }
 
     private void invisibleVideoView() {
-        videoSelected.setLayoutParams(new FrameLayout.LayoutParams(0,0));
-    }
-
-    private void displayImage() {
-        invisibleVideoView();
-        visibleImageView();
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) frameLayout.getLayoutParams();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = 0;
+        layoutParams.weight = 0;
     }
 
     private void invisibleImageView() {
-        imageSelected.setLayoutParams(new LinearLayout.LayoutParams(0,0));
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) imageSelected.getLayoutParams();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = 0;
+        layoutParams.weight = 0;
     }
 
     private void visibleImageView() {
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                800);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) imageSelected.getLayoutParams();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = 0;
         layoutParams.gravity = Gravity.CENTER;
-        layoutParams.topMargin = 20;
+        layoutParams.weight = 7.0f;
         imageSelected.setLayoutParams(layoutParams);
     }
 
     private void visibleVideoView() {
-        frameLayout.setLayoutParams(new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                800
-        ));
-        videoSelected.setLayoutParams(new FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-        ));
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) frameLayout.getLayoutParams();
+        layoutParams.height = 0;
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.weight = 7.0f;
         mediaController = new MediaController(MainActivity.this);
+        mediaController.setAnchorView(frameLayout);
         videoSelected.setMediaController(mediaController);
     }
 
@@ -350,23 +358,47 @@ public class MainActivity extends AppCompatActivity {
         visibleVideoView();
     }
 
+    private void displayImage() {
+        invisibleVideoView();
+        visibleImageView();
+    }
+
     private void displayImageOrVideoReturnedFromGallery(Intent data) {
         Uri uri = data.getData();
-        String path = uri.getPath().toString();
-        String type = path.substring(path.indexOf("external")+9,path.indexOf("/media"));
+        String _path = uri.getPath().toString();
+        String type = _path.substring(_path.indexOf("external")+9,_path.indexOf("/media"));
         try {
             if(type.equals("images")) {
                 displayImage();
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                File des = createFile(TYPE_IMAGE);
+                OutputStream desStream = new FileOutputStream(des);
+                InputStream srcStream = this.getContentResolver().openInputStream(uri);
+                writeData(srcStream,desStream);
                 imageSelected.setImageBitmap(bitmap);
             }
             else if(type.equals("video")) {
+                File des = createFile(TYPE_VIDEO);
+                OutputStream desStream = new FileOutputStream(des);
+                InputStream srcStream = this.getContentResolver().openInputStream(uri);
+                writeData(srcStream,desStream);
                 displayVideo();
                 videoSelected.setVideoURI(uri);
-                videoSelected.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void writeData(InputStream src, OutputStream des) {
+        byte[] bytes = new byte[1024];
+        int len = 0;
+        try {
+            while((len =src.read(bytes))>0) {
+                des.write(bytes,0,len);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
